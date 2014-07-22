364b2a26be7aeae5959b52ac32237a1f7c07473a
pengfeis <supengfei007@gmail.com>
2014-07-22 23:00:01 +0800

    hello world

--- src/helloworld.pl
+++ src/helloworld.pl
@@ -8,18 +8,20 @@ my @scannedLinks;
 my @emails;
 my @linksWithEmail;
 
-my $mech = WWW::Mechanize->new( autocheck => 1 );
+my $mech = WWW::Mechanize->new( autocheck => 0 );
 my $input = "http://www.mininghome.com/mine_country_activity.html";
 
-fandLinksWithContry($input);
+open my $emailsFH, ">emails.txt";
+&fandLinksWithContry($input);
+print $emailsFH, @emails;
+
 sub fandLinksWithContry() {
 	my $inputUrl = shift;
 	$mech->get($inputUrl);
-	my @links = $mech->find_all_links(tag => "a", url_regex => qr/mine/i);
+	my @links = $mech->find_all_links(tag => "a", url_regex => qr/mine\&activity\=/i);
 	print scalar @links;
 	foreach ( @links ) {
 		my $url = $_;		# Get the link with contry name
-		print $url."\n";
 		findCompanyLink($url);
 	}
 }
@@ -27,6 +29,9 @@ sub fandLinksWithContry() {
 sub findCompanyLink() {
 	my $companyLink = shift;
 	$mech->get($companyLink);
+	unless ($mech->success) {
+		print "Can't get SU response from $companyLink->url_abs \n";
+	}
 	my @links = $mech->find_all_links(tag => "a", url_regex => qr/company\=mine\&companyid\=/i);
 	foreach	( @links ) {
 		my $url = $_;
@@ -37,9 +42,13 @@ sub findCompanyLink() {
 sub getEmailsFromPage() {
 	my $url = shift;
 	$mech->get($url);
-	my $content = $mech->content;
-	while ( $content =~ m/([^\s]+@[^\s]{2, 20}\.\w+)/ig) {
-		print $1."\n";
-		push @emails, $1;
+	my @tempEmails = $mech->find_all_links(tag => "a", url_regex => qr/mailto/i);
+	foreach my $email (@tempEmails) {
+		my $emailText = $email->text;
+		if ($emailText !~ m/^Contact Us|^Email Us/ig) {
+		print $emailText."\n";
+		push @emails, $emailText;
+		}
 	}
 }
+
